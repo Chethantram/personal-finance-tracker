c242b09b6f0bb8d6f6b81bda61ee60a614257b93
Chethan T <chethant91@gmail.com>
2025-06-05 08:37:48 +0530

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -24,34 +24,41 @@ const HeroSection = () => {
     };
   }, []);
   return (
-    <div className="px-5 md:px-14 lg:px-40">
-      <div>
-        <h1 className="text-9xl font-bold text-center gradient-to-b from-blue-800 to text-violet-500">
-          Manage Finance <br /> with AI Intelligence
+    <section className="pt-40 pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-to-b from-blue-800 to text-violet-500">
+          Manage Your Finances <br /> with Intelligence
         </h1>
-        <p className="text-xl font-semibold text-gray-800 text-center max-w-4xl mx-auto mt-8">
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
           SpendIQ is a personal finance management tool that helps you track
           your expenses, set budgets, and achieve your financial goals.
         </p>
-        <div className="flex justify-center gap-5 mt-8">
-         <Link href="/dashboard"> <Button className={"text-md rounded-sm"}>Get Started</Button></Link>
-          <Button className={"text-md rounded-md"} variant="outline">
-            Learn More
-          </Button>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="#">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
         </div>
       </div>
-      <div className="mt-8 mx-auto hero-image-wrapper">
-        <div ref={imageRef} className="hero-image">
-          <Image
-            src={"/banner.jpeg"}
-            alt="img"
-            width={1600}
-            height={1500}
-            className="object-cover rounded-lg"
-          />
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
